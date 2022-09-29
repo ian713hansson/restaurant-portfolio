ccfac27824d82943dcfbd979262b75ca160e144d
Ian Hansson <ian713hansson@gmail.com>
2022-09-29 11:11:59 -0500

    first bit

---
+++ restaurant-portfolio/src/components/Name.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Name = () => {
+    return (
+        <div class="deconstructed">
+  IAN HANSSON
+  <div>IAN HANSSON</div>
+  <div>IAN HANSSON</div>
+  <div>IAN HANSSON</div>
+  <div>IAN HANSSON</div>
+</div>
+    )
+}
+export default Name
---
+++ restaurant-portfolio/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Name from "../components/Name";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="name">
+                <Name />
+            </div>
+        </div>
+    )
+}
+
+export default Home
